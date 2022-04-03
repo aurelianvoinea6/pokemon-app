1afffc5f43922df33f7dceb923cfa360f1bc50c8
Aurelian <70028912+aurelianvoinea6@users.noreply.github.com>
2022-04-03 19:40:44 +0200

    added the onclick function to select the pokemon by id

--- app.js
+++ app.js
@@ -1,31 +1,34 @@
 const pokedex = document.getElementById("pokedex");
-console.log(pokedex);
+
 
 const fetchPokemon = async () =>{
     const url = `https://pokeapi.co/api/v2/pokemon?limit=150`;
     const res = await fetch(url);
     const data =  await res.json();
     const pokemon = data.results.map( (result, index) => ({
-        ...result, 
+        ... result, 
         id: index + 1,
         image: `https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/${index + 1}.png`
     }));
+    console.log(data.results);
     displayPokemon(pokemon);
-
 };
 
 const displayPokemon = (pokemon) => {
-    console.log(pokemon);
     const pokemonHTMLString = pokemon.map(pokeman => `
-        <li class="card ">
+        <li class="card" onclick="selectPokemon(${pokeman.id})">
             <img class="card-image" src="${pokeman.image}"/>
             <h2 class=""card-title>${pokeman.id}. ${pokeman.name}</h2>
-            
-        </li>
-    `)
-    .join("");
+        </li>`
+        )
+    .join('');
     pokedex.innerHTML = pokemonHTMLString;
-}
+};
+
+const selectPokemon = async (id) =>{
+    console.log(id);
+};
+
 
 
 fetchPokemon();
