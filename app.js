ba2b15f7759b2bde8ac8845f976ed0b169a4dbf7
Aurelian <70028912+aurelianvoinea6@users.noreply.github.com>
2022-04-03 13:29:43 +0200

    fetching data from the api

--- app.js
+++ app.js
@@ -1,1 +1,14 @@
-console.log("hello world");
+const fetchPokemon = () =>{
+   const url = `https://pokeapi.co/api/v2/pokemon/1`;
+   fetch(url)
+    .then( res => {
+        return res.json();
+    })
+    .then ( data => {
+        console.log(data);
+    });
+        
+};
+
+
+fetchPokemon();
